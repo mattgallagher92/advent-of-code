668a1da91786b42cd0a67590759b2ccda1bd379b
Matt Gallagher <mjg1992@hotmail.co.uk>
2023-12-02 18:11:54 +0000

    Add Day1.PartTwo solution

--- 2023.fsx
+++ 2023.fsx
@@ -13,4 +13,53 @@ module Day1 =
             |> System.IO.File.ReadLines
             |> Seq.sumBy toCalibrationValue
 
-Day1.PartOne.solve ()
+    module PartTwo =
+        let solve () =
+            let toCalibrationValue (line: string) =
+                let digitRepresentations =
+                    [
+                        "0", 0
+                        "1", 1
+                        "2", 2
+                        "3", 3
+                        "4", 4
+                        "5", 5
+                        "6", 6
+                        "7", 7
+                        "8", 8
+                        "9", 9
+                        "one", 1
+                        "two", 2
+                        "three", 3
+                        "four", 4
+                        "five", 5
+                        "six", 6
+                        "seven", 7
+                        "eight", 8
+                        "nine", 9
+                    ]
+
+                let indices =
+                    digitRepresentations
+                    |> List.map (fun (rep, i) -> i, line.IndexOf rep, line.LastIndexOf rep)
+
+                let firstDigit =
+                    indices
+                    |> List.filter (fun (_, first, _) -> first >= 0)
+                    |> List.minBy (fun (_, first, _) -> first)
+                    |> fun (i, _, _) -> i
+
+                let lastDigit =
+                    indices
+                    |> List.filter (fun (_, last, _) -> last >= 0)
+                    |> List.maxBy (fun (_, _, last) -> last)
+                    |> fun (i, _, _) -> i
+
+                10 * firstDigit + lastDigit
+
+            // FSI process has to run in same directory as this .fsx file for the relative path to work correctly.
+            "./day1input"
+            |> System.IO.File.ReadLines
+            |> Seq.sumBy toCalibrationValue
+
+Day1.PartTwo.solve ()
