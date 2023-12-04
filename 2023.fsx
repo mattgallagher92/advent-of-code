48ab0e97aeed8b1c462610d20cd8565529922ffd
Matt Gallagher <mjg1992@hotmail.co.uk>
2023-12-04 19:09:06 +0000

    Add Day4.PartTwo solution

--- 2023.fsx
+++ 2023.fsx
@@ -267,7 +267,34 @@ module Day4 =
                 card.MatchingNumberCount
                 |> fun count -> if count = 0 then 0 else Math.integerPower 2 (count - 1))
 
+    module PartTwo =
+        let solve lines =
+            let contributions = Collections.Generic.Dictionary<int,int>()
+
+            lines
+            |> Seq.map parse
+            // We can determine each card's contribution to the total by working out which copies it produces and using
+            // the contributions of later cards. This means that if we go through the sequence backwards then we won't
+            // have to keep track of the number of copies of each card, which should be simpler.
+            |> Seq.rev
+            |> Seq.sumBy (fun card ->
+                let copyNumbers = Seq.init card.MatchingNumberCount (fun i -> card.Number + 1 + i)
+                let contributionsFromCopies = copyNumbers |> Seq.sumBy (fun num -> contributions.Item num)
+                let contribution = 1 + contributionsFromCopies
+
+                contributions.Add(card.Number, contribution)
+
+                contribution)
+
 // FSI process has to run in same directory as this .fsx file for the relative path to work correctly.
 "./day4input"
 |> System.IO.File.ReadAllLines
-|> Day4.PartOne.solve
+// [
+//     "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
+//     "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19"
+//     "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1"
+//     "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83"
+//     "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36"
+//     "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"
+// ]
+|> Day4.PartTwo.solve
