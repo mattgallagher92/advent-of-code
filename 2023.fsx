bf4421bd448a8d51888e58e6f64b2e5b4403ffd7
Matt Gallagher <mjg1992@hotmail.co.uk>
2023-12-04 12:22:03 +0000

    Add Day4.PartOne solution

--- 2023.fsx
+++ 2023.fsx
@@ -1,5 +1,16 @@
 open System
 
+[<AutoOpen>]
+module Util =
+    [<RequireQualifiedAccess>]
+    module Math =
+        let integerPower base' exponent =
+            if exponent < 0 then
+                invalidArg (nameof(exponent)) $"exponent must be at least 0; given %i{exponent}"
+            else
+                Seq.replicate exponent base'
+                |> Seq.fold (*) 1
+
 module Day1 =
     module PartOne =
         let solve lines =
@@ -218,7 +229,48 @@ module Day3 =
 
             Seq.sum gearRatios
 
+module Day4 =
+    module PartOne =
+        open FParsec
+
+        type Card = {
+            Number: int
+            WinningNumbers: int Set
+            NumbersYouHave: int Set
+        }
+
+        module Card =
+            let points card =
+                Set.intersect card.WinningNumbers card.NumbersYouHave
+                |> Set.count
+                |> fun winningNumbersYouHave ->
+                    if winningNumbersYouHave = 0 then
+                        0
+                    else
+                        Math.integerPower 2 (winningNumbersYouHave - 1)
+
+        let pCardNumber = pstring "Card" >>. spaces1 >>. pint32 .>> pstring ":"
+        // >>? results in lookahead behaviour, meaning that the '|' won't cause an error.
+        // See http://www.quanttec.com/fparsec/users-guide/looking-ahead-and-backtracking.html
+        let pNumbers = many (spaces1 >>? pint32)
+        let pCard = pCardNumber .>>. pNumbers .>> pstring " |" .>>. pNumbers
+
+        let parse line =
+            match CharParsers.run pCard line with
+            | Success (((cardNumber, winningNumbers), numbersYouHave), _, _) ->
+                {
+                    Number = cardNumber
+                    WinningNumbers = Set winningNumbers
+                    NumbersYouHave =  Set numbersYouHave
+                }
+            | Failure (error, _, _) ->
+                failwith $"Failed to parse line (%s{error}): %s{line}"
+        let solve lines =
+            lines
+            |> Seq.map parse
+            |> Seq.sumBy Card.points
+
 // FSI process has to run in same directory as this .fsx file for the relative path to work correctly.
-"./day3input"
+"./day4input"
 |> System.IO.File.ReadAllLines
-|> Day3.PartTwo.solve
+|> Day4.PartOne.solve
