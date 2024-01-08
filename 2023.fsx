0933cb6a365b8c7315428fbce36bad104fa65fdc
Matt Gallagher <mjg1992@hotmail.co.uk>
2024-01-08 17:19:33 +0000

    Add Day20.PartOne solution

--- 2023.fsx
+++ 2023.fsx
@@ -1300,7 +1300,6 @@ module Day11 =
     module PartOne =
         let solve = solve 2
 
-
     module PartTwo =
         let solve = solve 1_000_000L
 
@@ -2413,6 +2412,180 @@ module Day19 =
 
         let solve text = text |> parse |> fst |> acceptedRanges
 
-"./input/2023/day19"
-|> System.IO.File.ReadAllText
-|> Day19.PartTwo.solve
+module Day20 =
+    open System.Collections.Generic
+
+    type Pulse =
+        | LowPulse
+        | HighPulse
+
+    type FlipFlopModule =
+        {
+            IsOn: bool
+        }
+
+        member this.HandleInput =
+            function
+            | LowPulse ->
+                let nowOn = not this.IsOn
+                let output = if nowOn then HighPulse else LowPulse
+                { IsOn = nowOn }, Some output
+            | HighPulse  ->
+                this, None
+
+
+    type ConjunctionModule =
+        {
+            // TODO: map might be slow with lots of connections. Consider dictionary.
+            InputPulses: Map<string, Pulse>
+        }
+
+        member this.HandleInput connectionLabel pulse =
+            let newInputs = this.InputPulses |> Map.add connectionLabel pulse
+
+            let output =
+                if newInputs |> Map.forall (fun _ pulse -> pulse = HighPulse) then
+                    LowPulse
+                else
+                    HighPulse
+
+            { InputPulses = newInputs },  output
+
+    type Module =
+        | FlipFlop of FlipFlopModule
+        | Conjunction of ConjunctionModule
+        | Broadcast
+
+    module Module =
+
+        let newFlipFlop = FlipFlop { IsOn = false }
+
+        let newConjunction inputConnectionLabels =
+            Conjunction { InputPulses = inputConnectionLabels |> Seq.map (fun l -> l, LowPulse) |> Map }
+
+        let handleInput outputLabel pulse =
+            function
+            | FlipFlop ff ->
+                let newState, output = ff.HandleInput pulse
+                FlipFlop newState, output
+            | Conjunction c ->
+                let newState, output = c.HandleInput outputLabel pulse
+                Conjunction newState, Some output
+            | Broadcast ->
+                Broadcast, Some pulse
+
+    type Connection = {
+        /// The label of the module whose output the connection attaches to.
+        OutputLabel: string
+        /// The label of the module whose input the connection attaches to.
+        InputLabel: string
+    }
+
+    /// Given a configuration before a button press, returns the configuration after the pulses have stopped being
+    /// sent, and a list of sent pulses that were sent paired with the connections that they were sent on.
+    let handleButtonPress (configuration: Dictionary<string, Module * Connection array>) =
+
+        let newConfig = Dictionary(configuration)
+
+        let queue = Queue<Pulse * Connection>()
+
+        let tryDequeue () =
+            match queue.TryDequeue() with
+            | true, item -> Some item
+            | false, _ -> None
+
+        queue.Enqueue(LowPulse, { OutputLabel = "button"; InputLabel = "broadcaster" })
+
+        let signals =
+            ()
+            |> List.unfold (fun _ ->
+                tryDequeue ()
+                |> Option.map (fun item ->
+
+                    let pulse, { OutputLabel = outputLabel; InputLabel = inputLabel } = item
+
+                    newConfig.TryGet inputLabel
+                    |> Option.iter (fun (module', outputConnections) ->
+
+                        let module'', output = module' |> Module.handleInput outputLabel pulse
+
+                        newConfig.[inputLabel] <- (module'', outputConnections)
+
+                        output
+                        |> Option.iter (fun output ->
+                            outputConnections |> Array.iter (fun conn -> queue.Enqueue(output, conn))))
+
+                    Some item, ()))
+            |> List.choose id
+
+        newConfig, signals
+
+    let parse (lines: string array) =
+
+        // Turn into more structured data.
+        let data =
+            lines
+            |> Array.map (fun line ->
+                let parts = line.Split(" -> ")
+                let typeSignifier, moduleLabel =
+                    match parts.[0].[0] with
+                    | '%' -> Some '%', parts.[0].[ 1 .. ]
+                    | '&' -> Some '&', parts.[0].[ 1 .. ]
+                    | _ -> None, parts.[0]
+                let outputConnectionLabels = parts.[1].Split(", ")
+                moduleLabel, typeSignifier, outputConnectionLabels)
+
+        // First pass to build up dictionary from module label to input labels.
+        let toInputLabels =
+            data
+            |> Array.collect (fun (moduleLabel, _, outputConnectionLabels) ->
+                outputConnectionLabels |> Array.map (fun outputLabel -> outputLabel, moduleLabel))
+            |> Array.groupBy fst
+            |> Seq.map (fun (k, vs) -> k, vs |> Array.map snd)
+            |> Map
+            |> Dictionary
+
+        // Second pass to create config using map.
+        data
+        |> Array.map (fun (moduleLabel, typeSignifier, outputConnectionLabels) ->
+
+            let module' =
+                match typeSignifier with
+                | Some '%' -> Module.newFlipFlop
+                | Some '&' -> Module.newConjunction toInputLabels.[moduleLabel]
+                | Some _ -> failwith "Bug"
+                | None -> Broadcast
+
+            let connections =
+                outputConnectionLabels |> Array.map (fun l -> { OutputLabel = moduleLabel; InputLabel = l })
+
+            moduleLabel, (module', connections))
+        |> Map
+        |> Dictionary
+
+    module PartOne =
+
+        let handleNButtonPresses n config =
+
+            let rec inner remaining config signals =
+
+                if remaining = 0 then
+                    config, signals |> Seq.toArray
+                else
+                    let newConfig, newSignals = config |> handleButtonPress
+                    inner (remaining - 1) newConfig (Seq.append signals newSignals)
+
+            inner n config Seq.empty
+
+        let solve lines =
+
+            let pulses = lines |> parse |> handleNButtonPresses 1000 |> snd |> Array.map fst
+
+            pulses
+            |> Seq.countBy id
+            |> Seq.map snd
+            |> Seq.reduce (*)
+
+"./input/2023/day20"
+|> System.IO.File.ReadAllLines
+|> Day20.PartOne.solve
