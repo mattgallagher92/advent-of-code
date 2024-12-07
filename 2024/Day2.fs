d531333f123e46b8b48975c479a0396e69936ffb
Matt Gallagher <mjg1992@hotmail.co.uk>
2024-12-07 16:32:42 +0000

    Add 2024 Day2.PartTwo solution

--- 2024/Day2.fs
+++ 2024/Day2.fs
@@ -2,19 +2,21 @@ module Day2
 
 open System
 
+let parse (lines: string array) =
+    lines
+    |> Array.map (fun line ->
+        line.Split " "
+        |> FsToolkit.ErrorHandling.Array.traverseOptionM Int32.tryParse
+        |> Option.defaultWith (fun _ -> failwith $"Invalid line %s{line}"))
+
+type ReportClassificatoin =
+    | Safe
+    | Unsafe
+
+let solve classify (lines: string array) =
+    lines |> parse |> Array.countBy classify |> Array.find (fst >> (=) Safe) |> snd
+
 module PartOne =
-    open FsToolkit.ErrorHandling
-
-    let parse (lines: string array) =
-        lines
-        |> Array.map (fun line ->
-            line.Split(" ")
-            |> Array.traverseOptionM Int32.tryParse
-            |> Option.defaultWith (fun _ -> failwith $"Invalid line %s{line}"))
-
-    type ReportClassificatoin =
-        | Safe
-        | Unsafe
 
     let classify (report: int array) =
         let differences = report |> Array.pairwise |> Array.map (fun (a, b) -> b - a)
@@ -30,8 +32,21 @@ module PartOne =
         else
             Unsafe
 
-    let solve (lines: string array) =
-        lines |> parse |> Array.countBy classify |> Array.find (fst >> (=) Safe) |> snd
+    let solve = solve classify
+
+module PartTwo =
+
+    let classify (report: int array) =
+        [|
+            for i in 0 .. report.Length - 1 do
+                report |> Array.removeAt i
+        |]
+        |> Array.exists (PartOne.classify >> (=) Safe)
+        |> function
+            | true -> Safe
+            | false -> Unsafe
+
+    let solve = solve classify
 
 module Test =
     open Expecto
@@ -64,16 +79,25 @@ module Test =
                 sampleReport6
             |]
 
-            testCase "PartOne.parse works on sample input" (fun _ ->
-                test <@ PartOne.parse sampleInput = sampleReports @>)
+            testCase "parse works on sample input" (fun _ -> test <@ parse sampleInput = sampleReports @>)
 
             testCase "PartOne.classify works on sample input" (fun _ ->
-                test <@ PartOne.classify sampleReport1 = PartOne.Safe @>
-                test <@ PartOne.classify sampleReport2 = PartOne.Unsafe @>
-                test <@ PartOne.classify sampleReport3 = PartOne.Unsafe @>
-                test <@ PartOne.classify sampleReport4 = PartOne.Unsafe @>
-                test <@ PartOne.classify sampleReport5 = PartOne.Unsafe @>
-                test <@ PartOne.classify sampleReport6 = PartOne.Safe @>)
+                test <@ PartOne.classify sampleReport1 = Safe @>
+                test <@ PartOne.classify sampleReport2 = Unsafe @>
+                test <@ PartOne.classify sampleReport3 = Unsafe @>
+                test <@ PartOne.classify sampleReport4 = Unsafe @>
+                test <@ PartOne.classify sampleReport5 = Unsafe @>
+                test <@ PartOne.classify sampleReport6 = Safe @>)
 
             testCase "PartOne.solve works on sample input" (fun _ -> test <@ PartOne.solve sampleInput = 2 @>)
+
+            testCase "PartTwo.classify works on sample input" (fun _ ->
+                test <@ PartTwo.classify sampleReport1 = Safe @>
+                test <@ PartTwo.classify sampleReport2 = Unsafe @>
+                test <@ PartTwo.classify sampleReport3 = Unsafe @>
+                test <@ PartTwo.classify sampleReport4 = Safe @>
+                test <@ PartTwo.classify sampleReport5 = Safe @>
+                test <@ PartTwo.classify sampleReport6 = Safe @>)
+
+            testCase "PartTwo.solve works on sample input" (fun _ -> test <@ PartTwo.solve sampleInput = 4 @>)
         ]
--- 2024/Program.fs
+++ 2024/Program.fs
@@ -8,5 +8,6 @@ let main args =
     let input = "./input/day2" |> System.IO.File.ReadAllLines
 
     input |> Day2.PartOne.solve |> printfn "Day 2 part one: %A"
+    input |> Day2.PartTwo.solve |> printfn "Day 2 part two: %A"
 
     0
