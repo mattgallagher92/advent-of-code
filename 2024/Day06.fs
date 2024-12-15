68c20bebbf4a34498ffcb5d738eb1a5869046f7b
Matt Gallagher <mjg1992@hotmail.co.uk>
2024-12-15 21:52:41 +0000

    wip

--- 2024/Day06.fs
+++ 2024/Day06.fs
@@ -85,69 +85,71 @@ module PartOne =
 
 module PartTwo =
 
-    /// The guard states split into sections where the guard is moving in one direction.
-    let straights guardStates =
-        guardStates
-        |> Array.eachWithNext
-        // Split when the current state's heading is different to the next state's heading.
-        |> Seq.splitAfterMatches (fun (curr, next) ->
-            match next with
-            | Some { Heading = h } when h <> curr.Heading -> true
-            | Some _
-            | None -> false)
-        |> Seq.map (Seq.map fst >> Seq.toArray)
-        |> Seq.toArray
-
-    /// The straight extended backwards until an obstacle is hit or the map is left.
-    let extend map straight =
-        let { Heading = d } as first = Array.head straight
-
-        let prev state =
-            let posBehind = MappedAreaState.posBehind state
-
-            posBehind
-            |> Array2D.tryGet map
-            |> Option.bind (fun c ->
-                match c with
-                | '#' -> None
-                | _ -> Some { Position = posBehind; Heading = d })
-
-        let generator x = x |> Option.map (fun x -> x, prev x)
-        // Go backwards one step at a time from first, then reverse the list so that it's as if it's going forwards.
-        let extension = List.unfold generator (Some first) |> List.rev
-        // Join the extension, which includes the first item of the straight to the rest of the straight.
-        Array.append (extension |> List.toArray) (Array.tail straight)
+    open System.Collections.Generic
 
     // TODO: This doesn't work; it sends onto another point on the path, but need to only send to already visited points. New plan:
+    let validObstacleLocations map =
+        let rows = map |> Array2D.rows
+        let cols = map |> Array2D.columns
+        let initialPos = map |> Array2D.findIndex ((=) '^')
+
+        // TODO: could replace with function state -> bool???
+        let statesThatWouldCauseLoop = HashSet()
+        let viableObstacleLocations = HashSet()
+
+        let mutable guardState = InMap { Position = initialPos; Heading = Up }
+
+        let isInMap =
+            function
+            | InMap _ -> true
+            | LeftMap -> false
+
+        while isInMap guardState do
+            guardState <-
+                match guardState with
+                | InMap state ->
+                    let posAhead = MappedAreaState.posAhead state
+
+                    if Array2D.isWithinBounds map posAhead then
+                        if posAhead ||> Array2D.get map = '#' then
+                            match state.Heading with
+                            | Up ->
+                                let currentRowIx, currentColIx = state.Position
+                                let currentCol = cols[currentColIx]
+                                // TODO: cache obstacle locations for quick lookup.
+                                let nextObstacleDownRowIx =
+                                    currentCol
+                                    |> Array.indexed
+                                    |> Array.tryFind (fun (i, c) -> i > currentRowIx && c = '#')
+                                    |> Option.map fst
+
+                                let maxRowIx = nextObstacleDownRowIx |> Option.defaultValue (currentCol.Length - 1)
+
+                                for r in currentRowIx..maxRowIx do
+                                    statesThatWouldCauseLoop.Add {
+                                        Position = r, currentColIx
+                                        Heading = Left
+                                    }
+                                    |> ignore
+                            | Right -> TODO
+                            | Down -> TODO
+                            | Left -> TODO
+
+                            InMap {
+                                state with
+                                    Heading = Direction.next state.Heading
+                            }
+                        else
+                            InMap { state with Position = posAhead }
+                    else
+                        LeftMap
+                | LeftMap -> LeftMap
+
+        [||]
     // Make a note of obstacles that have been hit so far.
     // Use that to calculate the places where turning back in that direction would cause the obstacle to be hit in the same way (along the straight).
     // Turn that into a list of states where a turn would cause a loop.
     // For every point along the path, check if it's one of those points.
-    let validObstacleLocations map =
-        let allGuardStates = map |> PartOne.allGuardStates
-        let extendedStraights = allGuardStates |> straights |> Array.map (extend map)
-        let initialPos = allGuardStates |> Array.head |> _.Position
-
-        let posToWalkedStates = allGuardStates |> Array.groupBy _.Position |> dict
-
-        let posToStraightStates =
-            extendedStraights |> Array.collect id |> Array.groupBy _.Position |> dict
-
-        // A valid location for an obstacle is just ahead of the places where the guard's regular path intersects one
-        // of the extended straights and the guard is heading in the previous direction to the straight's direction,
-        // so long as that location is not the guard's start position.
-        allGuardStates
-        |> Array.filter (fun x -> x.Position <> initialPos)
-        |> Array.choose (fun walkedState ->
-            let straightHeadings =
-                posToStraightStates[walkedState.Position] |> Array.map _.Heading
-
-            printfn $"%A{walkedState.Position} %A{walkedState.Heading}, straights: %A{straightHeadings}"
-
-            if Array.contains (Direction.next walkedState.Heading) straightHeadings then
-                Some(MappedAreaState.posAhead walkedState)
-            else
-                None)
 
     let solve lines =
         lines |> parse |> validObstacleLocations |> Array.distinct |> Array.length
@@ -179,57 +181,10 @@ module Test =
             ]
 
             testList "PartTwo" [
-                let moveUp col =
-                    Array.map (fun row -> { Position = row, col; Heading = Up })
-
-                let moveRight row =
-                    Array.map (fun col -> { Position = row, col; Heading = Right })
-
-                let moveDown col =
-                    Array.map (fun row -> { Position = row, col; Heading = Down })
-
-                let moveLeft row =
-                    Array.map (fun col -> { Position = row, col; Heading = Left })
-
-                testCase "straights works with sample map" (fun _ ->
-                    let allGuardStates = PartOne.allGuardStates sampleMap
-
-                    let result = PartTwo.straights allGuardStates
-
-                    let expected = [|
-                        [| 6..-1..1 |] |> moveUp 4
-                        [| 4..+1..8 |] |> moveRight 1
-                        [| 1..+1..6 |] |> moveDown 8
-                        [| 8..-1..2 |] |> moveLeft 6
-
-                        [| 6..-1..4 |] |> moveUp 2
-                        [| 2..+1..6 |] |> moveRight 4
-                        [| 4..+1..8 |] |> moveDown 6
-                        [| 6..-1..1 |] |> moveLeft 8
-
-                        [| 8..-1..7 |] |> moveUp 1
-                        [| 1..+1..7 |] |> moveRight 7
-                        [| 7..+1..9 |] |> moveDown 7
-                    |]
-
-                    test <@ result = expected @>)
-
-                testCase "extend works with sample map and extension off map" (fun _ ->
-                    let straight = [| 6..-1..1 |] |> moveUp 4
-                    let result = PartTwo.extend sampleMap straight
-                    let expected = [| 9..-1..1 |] |> moveUp 4
-                    test <@ result = expected @>)
-
-                testCase "extend works with sample map and extension into obstacle" (fun _ ->
-                    let straight = [| 4..+1..9 |] |> moveRight 6
-                    let result = PartTwo.extend sampleMap straight
-                    let expected = [| 2..+1..9 |] |> moveRight 6
-                    test <@ result = expected @>)
-
-                testCase "validObstacleLocations works with sample map" (fun _ ->
-                    let result = PartTwo.validObstacleLocations sampleMap
-                    let expected = [| 6, 3; 7, 6; 7, 7; 8, 1; 8, 3; 9, 7 |]
-                    test <@ Set result = Set expected @>)
+            // testCase "validObstacleLocations works with sample map" (fun _ ->
+            //     let result = PartTwo.validObstacleLocations sampleMap
+            //     let expected = [| 6, 3; 7, 6; 7, 7; 8, 1; 8, 3; 9, 7 |]
+            //     test <@ Set result = Set expected @>)
 
             // testCase "solve works with sample input" (fun _ -> test <@ PartTwo.solve sampleInput = 6 @>)
             ]
