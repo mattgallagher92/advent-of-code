bf9d2cd5791f63f8293eff1a6ec1c1d6b9eb0bf8
Matt Gallagher <mjg1992@hotmail.co.uk>
2024-12-26 15:31:04 +0000

    Add Day11.PartTwo.solve with caching (too slow)

--- 2024/Day11.fs
+++ 2024/Day11.fs
@@ -2,19 +2,18 @@ module Day11
 
 let parse (line: string) = line.Split ' ' |> Array.map int64
 
+let successors stone =
+    let engraving = string stone
+
+    match engraving.Length % 2 = 0, stone with
+    | _, 0L -> [| 1L |]
+    | true, _ ->
+        let firstHalf = engraving.Substring(0, engraving.Length / 2)
+        let secondHalf = engraving.Substring(engraving.Length / 2)
+        [| int firstHalf; int secondHalf |]
+    | false, _ -> [| stone * 2024L |]
+
 module PartOne =
-
-    let successors stone =
-        let engraving = string stone
-
-        match engraving.Length % 2 = 0, stone with
-        | _, 0L -> [| 1L |]
-        | true, _ ->
-            let firstHalf = engraving.Substring(0, engraving.Length / 2)
-            let secondHalf = engraving.Substring(engraving.Length / 2)
-            [| int firstHalf; int secondHalf |]
-        | false, _ -> [| stone * 2024L |]
-
     let iterate stones = stones |> Array.collect successors
 
     let iterateNTimes n =
@@ -25,7 +24,55 @@ module PartOne =
 
 module PartTwo =
 
-    let solve (lines: string array) = -1
+    open System.Collections.Generic
+
+    [<Literal>]
+    let maxBlinks = 75
+
+    /// Stone to list of arrays produced at each successive iteration, in reverse order.
+    let cache = Dictionary<int64, int64 array list>()
+
+    // TODO: add explanatory comments.
+    let rec stonesAfterBlinks n startStone =
+        match n, cache.TryGet startStone with
+        | 1, _ -> [ successors startStone ]
+        | _, Some result when result.Length >= n ->
+            // Want last n elements.
+            result |> List.skip (result.Length - n)
+        | _, Some result ->
+            let iterationsCached = result.Length
+
+            // TODO: this is still very slow when n is large because result.Head contains a lot of stones; it's growing as roughly 1.5 ^ n.
+            let lists = result.Head |> Array.map (stonesAfterBlinks (n - iterationsCached))
+
+            let newResult = [
+                // lists have length n - iterationsCached because they're produced by stonesAfterBlinks (n - iterationsCached).
+                for i in 1 .. (n - iterationsCached) do
+                    lists |> Array.map (fun l -> l[i - 1]) |> Array.reduce Array.append
+                yield! result
+            ]
+
+            cache[startStone] <- newResult
+            newResult
+        | _, None ->
+            let lists = startStone |> successors |> Array.map (stonesAfterBlinks (n - 1))
+
+            let result = [
+                for i in 1 .. n - 1 do
+                    lists |> Array.map (fun l -> l[i - 1]) |> Array.reduce Array.append
+                successors startStone
+            ]
+
+            cache[startStone] <- result
+            result
+
+    let solveN n (lines: string array) =
+        lines[0]
+        |> parse
+        |> Array.collect (stonesAfterBlinks n >> List.head)
+        |> Array.length
+
+    let solve = solveN 75
 
 module Test =
 
@@ -84,11 +131,58 @@ module Test =
                     test <@ after5 = expectedAfter5 @>
                     test <@ after6 = expectedAfter6 @>)
 
+
                 testCase "solve works with sample input" (fun _ -> test <@ PartOne.solve sampleInput = 55312 @>)
             ]
 
             testList "PartTwo" [
-                testCase "solve works with sample input" (fun _ -> test <@ PartTwo.solve sampleInput = -1 @>)
+
+                testCase "stoneAfterBlinks works with sample input" (fun _ ->
+
+                    let after n =
+                        [| 125L; 17L |]
+                        |> Array.map (PartTwo.stonesAfterBlinks 6)
+                        |> Array.collect (List.item (6 - n))
+
+                    let expectedAfter1 = [| 253000L; 1L; 7L |]
+                    let expectedAfter2 = [| 253L; 0L; 2024L; 14168L |]
+                    let expectedAfter3 = [| 512072L; 1L; 20L; 24L; 28676032L |]
+                    let expectedAfter4 = [| 512L; 72L; 2024L; 2L; 0L; 2L; 4L; 2867L; 6032L |]
+                    let expectedAfter5 = [| 1036288L; 7L; 2L; 20L; 24L; 4048L; 1L; 4048L; 8096L; 28L; 67L; 60L; 32L |]
+
+                    let expectedAfter6 = [|
+                        2097446912L
+                        14168L
+                        4048L
+                        2L
+                        0L
+                        2L
+                        4L
+                        40L
+                        48L
+                        2024L
+                        40L
+                        48L
+                        80L
+                        96L
+                        2L
+                        8L
+                        6L
+                        7L
+                        6L
+                        0L
+                        3L
+                        2L
+                    |]
+
+                    test <@ after 1 = expectedAfter1 @>
+                    test <@ after 2 = expectedAfter2 @>
+                    test <@ after 3 = expectedAfter3 @>
+                    test <@ after 4 = expectedAfter4 @>
+                    test <@ after 5 = expectedAfter5 @>
+                    test <@ after 6 = expectedAfter6 @>)
+
+                testCase "solveN works with sample input" (fun _ -> test <@ PartTwo.solveN 25 sampleInput = 55312 @>)
             ]
         ]
 
