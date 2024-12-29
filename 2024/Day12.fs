7d9da95adf481f861c2b3c189663e0844fce5cc8
Matt Gallagher <mjg1992@hotmail.co.uk>
2024-12-29 12:15:07 +0000

    Remove notes for unselected Day12 options

--- 2024/Day12.fs
+++ 2024/Day12.fs
@@ -3,28 +3,6 @@ module Day12
 module PartOne =
 
     // Plan:
-    // - Repeat following until whole map is covered to find all regions.
-    //   - Take left-most plot from top-most uncovered row.
-    //   - Depth- or breadth-first search to find region of same plants (TEST).
-    // - Area is number of plots in region.
-    // - Find perimeter by either:
-    //   1. Adding exterior perimeter to interior perimeters, which are exterior perimeters of contained regions.
-    //     - First find perimeters of contained regions (recurse).
-    //     - Find exterior perimeter by boundary tracing (TEST):
-    //       - Start at left-most top-most contained plot.
-    //       - Go right until the next plot to the right is not in the shape (trace a top edge).
-    //       - Go down until the next plot below is not in the shape (trace a right edge).
-    //       - Go left until the next plot to the left is not in the shape (trace a bottom edge).
-    //       - Go up until the next plot above is not in the shape (trace a left edge).
-    //       - Stop when back at start position.
-    //     - Add perimeter contributions.
-    //     - O(rp) where region size and p is perimeter length (for each plot on perimeter, check set inclusion for next plot)
-    //   2. Summing perimeter contributions from every plot
-    //     - Perimeter contribution from each plot is number of adjacent plots that are not in the region.
-    //     - O(r^2) where r is region size (for each contained plot, checking set inclusion for each adjacent plot).
-    // - TODO: how to find contained regions? (TEST)
-    //
-    // Alternative:
     // - Scan through all plots, row-by-row, building up the following data for each plot:
     //   - For current plot: PlantType; RegionId; IsTopBoundary; IsLeftBoundary. Determine by checking plot above and plot to left.
     //   - For plot to left: IsRightBoundary. Also set for last plot in row.
