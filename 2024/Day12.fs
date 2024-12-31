7618fa26d6b2da5f7a0f259503508550dbfdf10e
Matt Gallagher <mjg1992@hotmail.co.uk>
2024-12-31 12:36:57 +0000

    Move calculateRegions out of PartOne module

--- 2024/Day12.fs
+++ 2024/Day12.fs
@@ -35,141 +35,142 @@ type Region = {
             |]
             |> Array.sum)
 
-module PartOne =
 
-    type private PreviousPlotData = {|
-        Coordinates: int * int
-        PlantType: char
-        RegionId: int * int
-        IsTopBoundary: bool
-        IsLeftBoundary: bool
-    |}
+type private PreviousPlotData = {|
+    Coordinates: int * int
+    PlantType: char
+    RegionId: int * int
+    IsTopBoundary: bool
+    IsLeftBoundary: bool
+|}
 
-    type private EarlierPlotData = {|
-        Coordinates: int * int
-        PlantType: char
-        RegionId: int * int
-        IsTopBoundary: bool
-        IsLeftBoundary: bool
-        IsRightBoundary: bool
-    |}
+type private EarlierPlotData = {|
+    Coordinates: int * int
+    PlantType: char
+    RegionId: int * int
+    IsTopBoundary: bool
+    IsLeftBoundary: bool
+    IsRightBoundary: bool
+|}
 
-    type private FoldState = {
-        Previous: PreviousPlotData
-        Earlier: EarlierPlotData array
-        Complete: CompletePlotData array
-        RegionIdUpdates: ((int * int) * (int * int)) list
-    }
+type private FoldState = {
+    Previous: PreviousPlotData
+    Earlier: EarlierPlotData array
+    Complete: CompletePlotData array
+    RegionIdUpdates: ((int * int) * (int * int)) list
+}
 
-    let calculateRegions map =
-        if map |> Array2D.length2 < 2 then
-            failwith "Code assumes map has width at least 2"
+let calculateRegions map =
+    if map |> Array2D.length2 < 2 then
+        failwith "Code assumes map has width at least 2"
 
-        let initialState =
-            (map, 0, 0)
-            |||> Array2D.get
-            |> fun plant -> {
-                Previous = {|
-                    Coordinates = 0, 0
-                    PlantType = plant
-                    RegionId = 0, 0
-                    IsTopBoundary = true
-                    IsLeftBoundary = true
-                |}
-                Earlier = [||]
-                Complete = [||]
-                RegionIdUpdates = []
-            }
+    let initialState =
+        (map, 0, 0)
+        |||> Array2D.get
+        |> fun plant -> {
+            Previous = {|
+                Coordinates = 0, 0
+                PlantType = plant
+                RegionId = 0, 0
+                IsTopBoundary = true
+                IsLeftBoundary = true
+            |}
+            Earlier = [||]
+            Complete = [||]
+            RegionIdUpdates = []
+        }
 
-        // Scan through all plots, row-by-row, building up the following data for each plot:
-        //   - For current plot: PlantType; RegionId; IsTopBoundary; IsLeftBoundary. Determine by checking plot above and plot to left.
-        //   - For plot to left: IsRightBoundary. Also set for last plot in row.
-        //   - For plot above: IsBottomBoundary. Also set for plots in last row.
-        (initialState, map |> Array2D.indexed |> Array.tail)
-        ||> Array.fold (fun state ((r, c), plant) ->
-            let abovePlant = (r - 1, c) ||> Array2D.tryGet map
-            let leftPlant = (r, c - 1) ||> Array2D.tryGet map
+    // Scan through all plots, row-by-row, building up the following data for each plot:
+    //   - For current plot: PlantType; RegionId; IsTopBoundary; IsLeftBoundary. Determine by checking plot above and plot to left.
+    //   - For plot to left: IsRightBoundary. Also set for last plot in row.
+    //   - For plot above: IsBottomBoundary. Also set for plots in last row.
+    (initialState, map |> Array2D.indexed |> Array.tail)
+    ||> Array.fold (fun state ((r, c), plant) ->
+        let abovePlant = (r - 1, c) ||> Array2D.tryGet map
+        let leftPlant = (r, c - 1) ||> Array2D.tryGet map
 
-            let isTopBoundary =
-                abovePlant |> Option.map ((<>) plant) |> Option.defaultValue true
+        let isTopBoundary =
+            abovePlant |> Option.map ((<>) plant) |> Option.defaultValue true
 
-            let isLeftBoundary =
-                leftPlant |> Option.map ((<>) plant) |> Option.defaultValue true
+        let isLeftBoundary =
+            leftPlant |> Option.map ((<>) plant) |> Option.defaultValue true
 
-            let earlierAbove, otherEarlier =
-                state.Earlier
-                |> Array.partition (fun e -> e.Coordinates = (r - 1, c))
-                |> Pair.mapFst Array.tryHead
+        let earlierAbove, otherEarlier =
+            state.Earlier
+            |> Array.partition (fun e -> e.Coordinates = (r - 1, c))
+            |> Pair.mapFst Array.tryHead
 
-            let newRegionId, regionIdToUpdate =
-                match isTopBoundary, isLeftBoundary with
-                | true, true -> (r, c), None
-                | true, false -> state.Previous.RegionId, None
-                // Since width is at least 2, earlierAbove is guaranteed to be Some if isTopBoundary is false.
-                | false, true -> earlierAbove.Value.RegionId, None
-                | false, false ->
-                    let above, left = earlierAbove.Value.RegionId, state.Previous.RegionId
-                    let new' = min above left
-                    let toUpdate = if above <> left then Some(max above left) else None
-                    new', toUpdate
+        let newRegionId, regionIdToUpdate =
+            match isTopBoundary, isLeftBoundary with
+            | true, true -> (r, c), None
+            | true, false -> state.Previous.RegionId, None
+            // Since width is at least 2, earlierAbove is guaranteed to be Some if isTopBoundary is false.
+            | false, true -> earlierAbove.Value.RegionId, None
+            | false, false ->
+                let above, left = earlierAbove.Value.RegionId, state.Previous.RegionId
+                let new' = min above left
+                let toUpdate = if above <> left then Some(max above left) else None
+                new', toUpdate
 
-            {
-                Previous = {|
-                    Coordinates = r, c
-                    PlantType = plant
-                    RegionId = newRegionId
-                    IsTopBoundary = isTopBoundary
-                    IsLeftBoundary = isLeftBoundary
-                |}
-                Earlier = [|
-                    {| state.Previous with IsRightBoundary = isLeftBoundary |}
-                    yield! otherEarlier
-                |]
-                Complete =
-                    earlierAbove
-                    |> Option.map (fun earlierAbove -> [|
-                        {| earlierAbove with IsBottomBoundary = isTopBoundary |}
-                        yield! state.Complete
-                    |])
-                    |> Option.defaultValue state.Complete
-                RegionIdUpdates =
-                    match regionIdToUpdate with
-                    | Some rId -> (rId, newRegionId) :: state.RegionIdUpdates
-                    | None -> state.RegionIdUpdates
-            })
-        |> fun state ->
-            let distinctUpdates =
-                state.RegionIdUpdates
-                |> List.toArray
-                // Reverse before and after distinct to keep later elements if there are duplicates.
-                |> Array.rev
-                |> Array.distinct
-                |> Array.rev
-
-            let updateRegionId (data: CompletePlotData) =
-                (data, distinctUpdates)
-                ||> Array.fold (fun c (regionIdToUpdate, newRegionId) ->
-                    if c.RegionId = regionIdToUpdate then
-                        {| c with RegionId = newRegionId |}
-                    else
-                        c)
-
-            (state.Complete, state.Earlier)
-            // Earlier is plots on bottom row except bottom-right.
-            ||> Array.fold (fun complete earlier -> [| {| earlier with IsBottomBoundary = true |}; yield! complete |])
-            |> fun rest -> [|
-                {|
-                    // Previous is bottom-right plot
-                    state.Previous with
-                        IsRightBoundary = true
-                        IsBottomBoundary = true
-                |}
-                yield! rest
+        {
+            Previous = {|
+                Coordinates = r, c
+                PlantType = plant
+                RegionId = newRegionId
+                IsTopBoundary = isTopBoundary
+                IsLeftBoundary = isLeftBoundary
+            |}
+            Earlier = [|
+                {| state.Previous with IsRightBoundary = isLeftBoundary |}
+                yield! otherEarlier
             |]
-            |> Array.map updateRegionId
-        // - Group by region ID to get regions.
-        |> Array.groupBy _.RegionId
-        |> Array.map (fun (regionId, plots) -> { RegionId = regionId; Plots = plots })
+            Complete =
+                earlierAbove
+                |> Option.map (fun earlierAbove -> [|
+                    {| earlierAbove with IsBottomBoundary = isTopBoundary |}
+                    yield! state.Complete
+                |])
+                |> Option.defaultValue state.Complete
+            RegionIdUpdates =
+                match regionIdToUpdate with
+                | Some rId -> (rId, newRegionId) :: state.RegionIdUpdates
+                | None -> state.RegionIdUpdates
+        })
+    |> fun state ->
+        let distinctUpdates =
+            state.RegionIdUpdates
+            |> List.toArray
+            // Reverse before and after distinct to keep later elements if there are duplicates.
+            |> Array.rev
+            |> Array.distinct
+            |> Array.rev
+
+        let updateRegionId (data: CompletePlotData) =
+            (data, distinctUpdates)
+            ||> Array.fold (fun c (regionIdToUpdate, newRegionId) ->
+                if c.RegionId = regionIdToUpdate then
+                    {| c with RegionId = newRegionId |}
+                else
+                    c)
+
+        (state.Complete, state.Earlier)
+        // Earlier is plots on bottom row except bottom-right.
+        ||> Array.fold (fun complete earlier -> [| {| earlier with IsBottomBoundary = true |}; yield! complete |])
+        |> fun rest -> [|
+            {|
+                // Previous is bottom-right plot
+                state.Previous with
+                    IsRightBoundary = true
+                    IsBottomBoundary = true
+            |}
+            yield! rest
+        |]
+        |> Array.map updateRegionId
+    // - Group by region ID to get regions.
+    |> Array.groupBy _.RegionId
+    |> Array.map (fun (regionId, plots) -> { RegionId = regionId; Plots = plots })
+
+module PartOne =
 
     let fencingPrice (region: Region) = region.Area * region.Perimeter()
 
@@ -211,7 +212,7 @@ module Test =
             testList "PartOne" [
                 let testAreasAndPerimeters mapName map expected =
                     testCase $"%s{mapName} regions have correct areas and perimeters" (fun _ ->
-                        let result = map |> PartOne.calculateRegions
+                        let result = map |> calculateRegions
 
                         test
                             <@
