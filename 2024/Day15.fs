17e1345b95f2d509625bb80d52c9311dff546221
Matt Gallagher <mjg1992@hotmail.co.uk>
2025-01-04 11:04:24 +0000

    Add Day15.PartTwo.solve

--- 2024/Day15.fs
+++ 2024/Day15.fs
@@ -1,7 +1,15 @@
 module Day15
 
-// TODO: would be nice to make into type extensions, but I can't find the right syntax.
+// TODO: would be nice to make into type extensions, but I haven't found the right syntax.
 let addPair (i2, j2) (i1, j1) = i1 + i2, j1 + j2
+
+let direction =
+    function
+    | '^' -> -1, 0
+    | '<' -> 0, -1
+    | 'v' -> 1, 0
+    | '>' -> 0, 1
+    | c -> failwith $"Invalid move char '{c}'"
 
 module PartOne =
 
@@ -11,22 +19,16 @@ module PartOne =
         |> fun (a, b) -> a |> Array.map Seq.toArray |> array2D, System.String.Join("", b) |> Seq.toArray
 
     let attemptMove map (r, c as pos) move =
-        let direction =
-            match move with
-            | '^' -> -1, 0
-            | '<' -> 0, -1
-            | 'v' -> 1, 0
-            | '>' -> 0, 1
-            | c -> failwith $"Invalid move char '{c}'"
+        let direction = direction move
 
         let untilStopChar =
             (pos, true)
-            |> Array.unfold (fun (pos, b) ->
+            |> Array.unfold (fun (p, b) ->
                 if not b then
                     None
                 else
-                    let x = pos ||> Array2D.get map
-                    Some((pos, x), (pos |> addPair direction, x <> '#' && x <> '.')))
+                    let x = p ||> Array2D.get map
+                    Some((p, x), (p |> addPair direction, x <> '#' && x <> '.')))
 
         match untilStopChar |> Array.last |> snd with
         | '#' -> map, pos
@@ -50,7 +52,93 @@ module PartOne =
 
 module PartTwo =
 
-    let solve (lines: string array) = -1
+    let expand =
+        function
+        | '#' -> [| '#'; '#' |]
+        | 'O' -> [| '['; ']' |]
+        | '.' -> [| '.'; '.' |]
+        | '@' -> [| '@'; '.' |]
+        | c -> failwith $"Invalid input char {c}"
+
+    let parse (lines: string array) =
+        lines
+        |> Array.splitAt (lines |> Array.findIndex System.String.IsNullOrEmpty)
+        |> fun (a, b) ->
+            a |> Array.map (Seq.collect expand >> Seq.toArray) |> array2D, System.String.Join("", b) |> Seq.toArray
+
+    let attemptMove map pos move =
+        let d = direction move
+
+        let acc =
+            ([| pos |], true)
+            |> Array.unfold (fun (ps, canContinue) ->
+                if not canContinue then
+                    None
+                else
+                    let mutable hitBox = false
+                    let mutable blocked = false
+
+                    let next =
+                        ps
+                        |> Array.collect (fun p ->
+                            let aheadPos = p |> addPair d
+
+                            match aheadPos ||> Array2D.get map with
+                            | '.' -> [||]
+                            | '[' ->
+                                hitBox <- true
+
+                                match d with
+                                | _, 0 -> [| aheadPos; aheadPos |> addPair (0, 1) |]
+                                | 0, 1
+                                | 0, -1 -> [| aheadPos |]
+                                | _ -> failwith $"Invalid direction vector %A{d}"
+                            | ']' ->
+                                hitBox <- true
+
+                                match d with
+                                | _, 0 -> [| aheadPos; aheadPos |> addPair (0, -1) |]
+                                | 0, 1
+                                | 0, -1 -> [| aheadPos |]
+                                | _ -> failwith $"Invalid direction vector %A{d}"
+                            | '#' ->
+                                blocked <- true
+                                [||]
+                            | c -> failwith $"Invalid ahead char {c}")
+                        // Can end up with duplicate positions if both the left and right hand side of the box are pushed.
+                        |> Array.distinct
+
+                    Some((ps, blocked), (next, hitBox && not blocked)))
+
+        let isBlocked = acc |> Array.last |> snd
+
+        if isBlocked then
+            map, pos
+        else
+            acc
+            |> Array.collect fst
+            |> fun positionsToMove -> positionsToMove, map
+            ||> Array.foldBack (fun (r, c as p) m ->
+                let p' = p |> addPair d
+                let r', c' = p'
+                m |> Array2D.updateAt r' c' (m[r, c]) |> Array2D.updateAt r c '.')
+            |> fun m -> m, pos |> addPair d
+
+    let solve (lines: string array) =
+        let sb = System.Text.StringBuilder()
+
+        let result =
+            lines
+            |> parse
+            |> Pair.mapFst (fun map -> map, map |> Array2D.findIndex ((=) '@'))
+            ||> Array.fold (fun (map, pos) move -> attemptMove map pos move)
+            |> fst
+            |> Array2D.findAllIndexes ((=) '[')
+            |> Array.sumBy (fun (r, c) -> 100 * r + c)
+
+        System.IO.File.WriteAllText("./maps.txt", sb.ToString())
+
+        result
 
 module Test =
 
@@ -102,7 +190,20 @@ module Test =
             ]
 
             testList "PartTwo" [
-                testCase "solve works with sample input" (fun _ -> test <@ PartTwo.solve smallSampleInput = -1 @>)
+                testCase "move up works with two boxes ahead" (fun _ ->
+                    let map =
+                        PartTwo.parse [| "###"; "#.#"; "#O#"; "#O#"; "#@#"; "###"; ""; "^" |] |> fst
+
+                    let initialPos = map |> Array2D.findIndex ((=) '@')
+                    let newMap, newPos = PartTwo.attemptMove map initialPos '^'
+
+                    let expectedMap =
+                        PartTwo.parse [| "###"; "#O#"; "#O#"; "#@#"; "#.#"; "###"; ""; "^" |] |> fst
+
+                    let expectedPos = expectedMap |> Array2D.findIndex ((=) '@')
+                    test <@ newMap = expectedMap && newPos = expectedPos @>)
+
+                testCase "solve works with sample input" (fun _ -> test <@ PartTwo.solve largeSampleInput = 9021 @>)
             ]
         ]
 
